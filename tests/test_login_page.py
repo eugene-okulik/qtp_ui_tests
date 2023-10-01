c4ea0abd36adfeaae8d4724c336922ce1f54914c
Eugeny Okulik <tiburon854@gmail.com>
2023-10-01 12:23:25 +0300

    marks added

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,3 +1,7 @@
+import pytest
+
+
+@pytest.mark.regression
 def test_incorrect_pass(login_page):
     login_page.open()
     login_page.fill_login_form(login='user@mail.com', passw='qwqwqwqw')
--- tests/test_sale_page.py
+++ tests/test_sale_page.py
@@ -1,3 +1,7 @@
+import pytest
+
+
+@pytest.mark.extended
 def test_sale_title(sale_page):
     sale_page.open()
     assert sale_page.page_title_text == 'Sale'
